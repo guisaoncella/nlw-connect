5432571d0f5fe447517579f73b117222f6761272
Guilherme Saoncella <guisaoncella@gmail.com>
2025-02-18 11:59:22 -0300

    setup
    create setup for the project and add a first route
    setup swagger and zod

---
+++ trilha-node/src/server.ts
@@ -0,0 +1,34 @@
+import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors'
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform } from 'fastify-type-provider-zod'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
+
+const app = fastify()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(subscribeToEventRoute)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('Server running on port 3333...')
+})
