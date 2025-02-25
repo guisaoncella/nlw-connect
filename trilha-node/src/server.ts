b5b18ac12d3079247114538d7bbc2d6a4c079062
Guilherme Saoncella <guisaoncella@gmail.com>
2025-02-25 10:36:28 -0300

    add top three ranking

--- trilha-node/src/server.ts
+++ trilha-node/src/server.ts
@@ -13,6 +13,8 @@ import { accessInviteLinkRoute } from './routes/access-invite-link-route'
 import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 import { getSubscriberInviteClicksRoute } from './routes/get-subscriber-invite-clicks-route'
 import { getSubscriberInvitesCountRoute } from './routes/get-subscriber-invites-count-route'
+import { getSubscriberRankingPositionRoute } from './routes/get-subscriber-ranking-position-route'
+import { getRankingRoute } from './routes/get-ranking-route'
 
 const app = fastify()
 
@@ -39,6 +41,8 @@ app.register(subscribeToEventRoute)
 app.register(accessInviteLinkRoute)
 app.register(getSubscriberInviteClicksRoute)
 app.register(getSubscriberInvitesCountRoute)
+app.register(getSubscriberRankingPositionRoute)
+app.register(getRankingRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`Server running on port ${env.PORT}...`)
